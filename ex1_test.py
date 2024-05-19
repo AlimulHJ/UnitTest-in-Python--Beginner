edbac50159e8b1b97256c010cae9c3d7517cf8fe
Alimul Hasan Jami <alimulhasan000@gmail.com>
2024-05-19 01:40:01 -0400

    2nd commit - files updated for testing

--- ex1_test.py
+++ ex1_test.py
@@ -1,5 +1,14 @@
 import write_your_name as name_point
 
+# def test_hi_my_name_is():
+#     assert len(name_point.hi_my_name_is())  > 1
+
 def test_hi_my_name_is():
-    assert len(name_point.hi_my_name_is())  > 1 
+    name1 = name_point.hi_my_name_is()
+    name2 = name_point.hi_my_name_is()
+    assert len(name1) > 0, "The entered 1st name is empty!"
+    assert len(name2) > 0, "The entered 2nd name is empty!"
+
+    print(f"Test passed! Your name is: {name1} & {name2}")
+
 
--- write_your_name.py
+++ write_your_name.py
@@ -1,2 +1,6 @@
+# def hi_my_name_is():
+#     return "Alimul" # Write your name inside the quotation marks
+
 def hi_my_name_is():
-    return "" # Write your name inside the quotation marks
+    name = input("Please enter your name: ")
+    return name
